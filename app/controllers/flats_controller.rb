bf2bd74be678db91679a700a8514047404ab1f15
Erwin Adorable <adorable.erwin@gmail.com>
2023-09-15 16:55:05 +0200

    added working simple Airbnb, added search route but not yet working

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,7 +14,7 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.save
-    redirect_to flats_path
+    redirect_to  flat_path(@flat.id)
   end
 
   def edit
@@ -27,9 +27,18 @@ class FlatsController < ApplicationController
     redirect_to  flat_path(@flat.id)
   end
 
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, notice: "Restaurant was successfully destroyed."
+  end
+
+  def search
+  end
+
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:picture_url, :name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
