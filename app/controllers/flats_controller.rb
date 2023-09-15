45515cc5de91e075dffa57d0e19eb1111f173c02
Erwin Adorable <adorable.erwin@gmail.com>
2023-09-15 12:38:08 +0200

    added edit and show options

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -17,6 +17,16 @@ class FlatsController < ApplicationController
     redirect_to flats_path
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to  flat_path(@flat.id)
+  end
+
   private
 
   def flat_params
